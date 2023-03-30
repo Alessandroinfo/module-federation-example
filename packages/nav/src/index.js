a65ae5d42f5921fc9ec7e401592be655d4408df3
Alessandro Russo <alessandro.russo@datlas.it>
2023-03-30 16:23:44 +0200

    First implementation

--- packages/nav/src/index.js
+++ packages/nav/src/index.js
@@ -1,6 +1,7 @@
 const greetings = () => {
-    const node = document.createElement('h1');
-    node.innerHTML = 'App Shell';
+    const node = document.createElement('div');
+    node.innerHTML = 'Header';
+    node.style = 'padding: 1em; box-sizing: border-box;display: flex;background: aquamarine;font-family:sans-serif;'
     return node;
 }
 
